015b561bfcc47ceae1211e88f6d37a7e1c19ae1d
Jan Ehrlich <126582876+jehrl@users.noreply.github.com>
2023-03-01 20:06:18 +0100

    Grid class functions putted together into one

--- minesweeper.py
+++ minesweeper.py
@@ -10,18 +10,16 @@ class Grid:
         grid = []
         for grid_possition in range(self.possitions):
             grid.append(grid_possition)
-        return grid
-    def create_mines_in_grid(self):
         mines_possitions = random.sample(range(self.possitions), 10)
-        return mines_possitions
-    def create_play_grid(self):
         play_grid = []
         for play_possition in range(self.possitions):
             if play_possition in mines_possitions:
                 play_grid.append(-1)
             else:
                 play_grid.append(play_possition)
-        return play_grid
-    print(play_grid)
+        return print(play_grid)
+    
 
 print("Welcome to Minseweeper game")
+
+Grid(100,10)
