b147f1323586ae4350278d076b0035bf8b4fd4f8
Jan Ehrlich <126582876+jehrl@users.noreply.github.com>
2023-03-04 12:56:06 +0100

    creating game graphics

--- minesweeper.py
+++ minesweeper.py
@@ -8,6 +8,7 @@ class Grid:
         self.rows = rows
         self.lines = lines
         self.play_grid = []
+        self.visual_grid = []
 
     def create_grid(self):
         grid = []
@@ -21,12 +22,9 @@ class Grid:
                 self.play_grid.append(play_possition)
         return print(self.play_grid)
     def create_visual_grid(self):
-        visual_grid = []
         for cell in self.play_grid:
-            print("\n")
             if type(cell) == str:
-                visual_grid.append(cell)
-                print(cell)
+                self.visual_grid.append(cell)
             else:
                 neighbors = []
                 if (cell)%self.lines == 0:
@@ -35,24 +33,17 @@ class Grid:
                     neighbors.append(cell-self.lines-1)
                     neighbors.append(cell+self.lines-1)
                     neighbors.append(cell+self.lines)
-                    print(cell)
-                    print(neighbors)
                 elif (cell)%(self.lines) == 1:
                     neighbors.append(cell+1)
                     neighbors.append(cell-self.lines+1)
                     neighbors.append(cell-self.lines)
                     neighbors.append(cell+self.lines+1)
                     neighbors.append(cell+self.lines)
-                    print(cell)
-                    print(neighbors)
                 else:
                     neighbors = [cell+1, cell-1, cell+self.lines+1, cell+self.lines-1, cell+self.lines, cell-self.lines+1, cell-self.lines-1, cell-self.lines]
-                    print(cell)
-                    print(neighbors)                    
                 mine_count = sum(1 for j in neighbors if j >= 0 and j < self.possitions and type(self.play_grid[j-1]) == str)
-                print(mine_count)
-                visual_grid.append(mine_count)
-        return print(visual_grid)
+                self.visual_grid.append(mine_count)
+        return self.visual_grid
     
 def game_round(play_grid, line, row):
     next_round_grid = play_grid
@@ -64,7 +55,17 @@ def game_round(play_grid, line, row):
     
 
 print("Welcome to Minseweeper game")
-
-my_grid = Grid(5,5,5)
+print("How big grid you wanna create?")
+rows = int(input("How many rows:\n"))
+lines = int(input("How many lines:\n"))
+mines = int(input("With how many mines:\n"))
+my_grid = Grid(rows,lines,mines)
 my_grid.create_grid()
-my_grid.create_visual_grid() 
+my_grid.create_visual_grid()
+print(my_grid.visual_grid)
+def game_round(actual_grid):
+    previous_row = 0
+    for line in range(my_grid.lines):
+        print(my_grid.visual_grid[previous_row:(previous_row + my_grid.lines)])
+        previous_row += my_grid.lines
+game_round(my_grid.visual_grid)
